f148e6bc541e0a59b4509b8daf5a59fe64a09800
Abhishek Jadhav <abhishek.jadhav64@gmail.com>
2018-10-20 16:43:04 +0530

    Initial Commit

--- Roll-E/Assets/Scripts/LevelManager.cs
+++ Roll-E/Assets/Scripts/LevelManager.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
-    
+    public Button start,how2,aboutme, exit;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        start.onClick.AddListener(StartGame);
+        how2.onClick.AddListener(How2Play);
+        aboutme.onClick.AddListener(AboutMe);
+        exit.onClick.AddListener(ExitGame);
+
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		
 	}
 
-    
+    void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    void How2Play()
+    {
+
+    }
+
+    void AboutMe()
+    {
+
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
--- Roll-E/Assets/Scripts/Player.cs
+++ Roll-E/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
@@ -15,14 +16,22 @@ public class Player : MonoBehaviour {
 
     public Rigidbody sphere;
 
+    public Button left, right;
+
     LevelManager levelManager;
+    Vector3 velocity = Vector3.zero;
+    public float smoothTime = 0.3f;
 
     // Use this for initialization
     void Start () {
         sphere = GetComponent<Rigidbody>();
 
         MakeRoad();
-	}
+
+        left.onClick.AddListener(OnLeftClick);
+        right.onClick.AddListener(OnRightClick);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,7 +39,7 @@ public class Player : MonoBehaviour {
         GetInput();
 
         sphere.velocity = new Vector3(speed,0,0);
-        wall.velocity = new Vector3(speed, 0, 0);
+        wall.velocity = new Vector3(speed - 0.5f, 0, 0);
 	}
 
     void Jump()
@@ -45,13 +54,80 @@ public class Player : MonoBehaviour {
             Jump();
             Debug.Log("Up pressed");
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+     
+    }
+
+    IEnumerable Wait()
+    {
+        yield return new WaitForSeconds(0.3f);   
+    }
+
+    void OnLeftClick()
+    {
+
+        if (sphere.transform.position.z >= 1f)
         {
 
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else
         {
 
+            Vector3 v = new Vector3(sphere.position.x, sphere.position.y, sphere.position.z + 1);
+
+            /*for (float i=0 ; i <= 1f ; i += .1f)
+            {
+
+                sphere.position = Vector3.MoveTowards(sphere.position, v, 0.1f);
+                //sphere.position = Vector3.Lerp(sphere.position, v, i);
+                //Wait();  
+            } */
+
+
+           
+
+            
+
+            sphere.transform.Translate(0, 0, 1);
+           
+
+            //float a = sphere.position.z;
+
+            //Vector3 b = new Vector3(sphere.position.x, sphere.position.y, 1);
+
+            //sphere.position = b;
+        }
+        
+    }
+
+    void OnRightClick()
+    {
+
+        if (sphere.transform.position.z <= -1f)
+        {
+
+        }
+        else
+        {
+
+            Vector3 v = new Vector3(sphere.position.x, sphere.position.y, sphere.position.z-1);
+            
+            /*for (float i=0; i <= 1f; i += .1f)
+            {
+                sphere.position = Vector3.Lerp(sphere.position, v, i);
+                Wait();
+            } */
+
+           // sphere.position = Vector3.SmoothDamp(sphere.position, v, ref velocity, 0f);
+
+            sphere.transform.Translate(0, 0, -1);
+            //Vector3 v = new Vector3(sphere.position.x, sphere.position.y, -1);
+            //sphere.position = Vector3.Lerp(sphere.position, v, 1f);
+
+            /*float a = sphere.position.z;
+
+            Vector3 b = new Vector3(sphere.position.x,sphere.position.y , (int)Mathf.Ceil(a));
+
+            sphere.position = b;*/
         }
     }
 
@@ -67,6 +143,12 @@ public class Player : MonoBehaviour {
                 SpawnBlock();
             }
 
+        }
+
+        if (other.tag.Equals("Enemy"))
+        {
+            Time.timeScale = 0;
+            Debug.Log("Game end");
         }
     }
 
@@ -94,9 +176,9 @@ public class Player : MonoBehaviour {
     }
 
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnColliderEnter(Collider collider)
     {
-        if (collision.collider.tag.Equals("Enemy"))
+        if (collider.tag.Equals("Enemy"))
         {
             Time.timeScale = 0;
             Debug.Log("Game end");
